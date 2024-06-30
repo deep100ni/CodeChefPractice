e18ba2336f2eb3ff85b60e808fe3da0c2b839f80
deep100ni <166514388+deep100ni@users.noreply.github.com>
2024-06-30 17:55:17 +0530

    Create MANGOLASSI.kt

---
+++ MANGOLASSI.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val X = readInt()
+    print(if(X>35) "yes" else "no")
+}
+
+private fun readStr() = readln() // string line
+private fun readInt() = readStr().toInt() // single int
+private fun readStrings() = readStr().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
