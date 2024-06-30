099c16e979ce4aeac8f8e65e2ae2d1b35da9d409
deep100ni <166514388+deep100ni@users.noreply.github.com>
2024-06-30 17:27:52 +0530

    Create LUCKYSEVEN.kt

---
+++ LUCKYSEVEN.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val S = readStr()
+    print(S[6])
+}
+
+private fun readStr() = readln() // string line
+private fun readInt() = readStr().toInt() // single int
+private fun readStrings() = readStr().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
