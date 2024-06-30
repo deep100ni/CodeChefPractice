7f487cd698f0cc512a8cec14ecad35006dae3d3b
deep100ni <166514388+deep100ni@users.noreply.github.com>
2024-06-30 17:29:52 +0530

    Create CHRISTGREET.kt

---
+++ CHRISTGREET.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val X = readInt()
+    if (X == 25) print("Christmas") else print("Ordinary")
+}
+
+private fun readStr() = readln() // string line
+private fun readInt() = readStr().toInt() // single int
+private fun readStrings() = readStr().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
