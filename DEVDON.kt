d449caf305026886393e3bdcd17deb435edb473e
deep100ni <166514388+deep100ni@users.noreply.github.com>
2024-06-30 17:01:22 +0530

    Create DEVDON.kt

---
+++ DEVDON.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val(X,Y) = readInts()
+    print(X*Y)
+}
+
+private fun readStr() = readln() // string line
+private fun readInt() = readStr().toInt() // single int
+private fun readStrings() = readStr().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
