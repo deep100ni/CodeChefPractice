8d6aa1fda52b19619e099951736814b295b595b8
deep100ni <166514388+deep100ni@users.noreply.github.com>
2024-06-30 17:32:51 +0530

    Create START01.kt

---
+++ START01.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print(readInt())
+}
+
+private fun readStr() = readln() // string line
+private fun readInt() = readStr().toInt() // single int
+private fun readStrings() = readStr().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
